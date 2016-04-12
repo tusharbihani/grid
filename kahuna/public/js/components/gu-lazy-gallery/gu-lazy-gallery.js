ce43abd7d3e03be1d0b76532116507ffa1c0c485
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-04-12 17:26:38 +0100

    bind gallery element to directive

--- kahuna/public/js/components/gu-lazy-gallery/gu-lazy-gallery.js
+++ kahuna/public/js/components/gu-lazy-gallery/gu-lazy-gallery.js
@@ -2,24 +2,56 @@ import angular from 'angular';
 import Rx from 'rx';
 
 import '../../util/rx';
+import template from './gu-lazy-gallery.html!text';
 
 export var lazyGallery = angular.module('gu.lazyGallery', [
     'util.rx'
 ]);
 
-lazyGallery.controller('GuLazyGalleryCtrl', function() {
-    let ctrl = this;
-});
+lazyGallery.controller('GuLazyGalleryCtrl', ['$scope', function($scope) {
+    let ctrl = this,
+        pos = 0;
 
-lazyGallery.directive('guLazyGallery', ['observeCollection$', function(observeCollection$) {
-    return {
-        restrict: 'E',
-        controller: 'GuLazyGalleryCtrl',
-        template: './gu-lazy-gallery.html',
-        link: function (scope, element, attrs, ctrl) {
+    // Set gallery
+    $scope.gallery = {};
 
+    function setTransform() {
+        $scope.gallery[0].style.transform = 'translate3d(' + (-pos * $scope.gallery[0].offsetWidth) + 'px,0,0)';
+    }
 
-            console.log(attrs);
-        }
+    ctrl.previousItem = function() {
+        pos = Math.max(pos - 1, 0);
+        setTransform();
+    };
+
+    ctrl.nextItem = function() {
+        pos = Math.min(pos + 1, $scope.gallery[0].children.length - 1);
+        setTransform();
     };
 }]);
+
+lazyGallery.directive('guLazyGallery', function() {
+    return {
+        restrict: 'E',
+        template: template,
+        controller: 'GuLazyGalleryCtrl',
+        controllerAs: 'ctrl',
+        scope: {
+            galleryItems: '=guLazyGalleryItems'
+        }
+    };
+});
+
+lazyGallery.directive('guLazyGalleryList', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            gallery: '=guLazyGalleryList'
+        },
+        link: function(scope, element) {
+            //Bind list items to scope.
+
+            scope.gallery = element;
+        }
+    };
+});
